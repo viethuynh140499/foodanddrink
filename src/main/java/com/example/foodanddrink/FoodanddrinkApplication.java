eb1ab41f03ce13241845a6096f3d44108e55a4c1
viet_Logigear <thitranlanhat@gmail.com>
2021-06-16 23:05:47 +0700

    create Employee

---
+++ src/main/java/com/example/foodanddrink/FoodanddrinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodanddrink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodanddrinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodanddrinkApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/foodanddrink/model/Employee.java
@@ -0,0 +1,22 @@
+package com.example.foodanddrink.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name="email_id")
+    private String emailId;
+}
